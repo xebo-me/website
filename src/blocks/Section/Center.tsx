5520d01691056249aaa9563b9c7a264e8b2ea58e
finest08 <mr.mitcholl@gmail.com>
2023-01-20 12:49:45 +1300

    changes to center section

--- src/blocks/Section/Center.tsx
+++ src/blocks/Section/Center.tsx
@@ -4,6 +4,7 @@ import Button from '@mui/material/Button';
 import CardMedia from '@mui/material/CardMedia';
 import Stack from '@mui/material/Stack';
 import Typography from '@mui/material/Typography';
+import Grid from "@mui/material/Unstable_Grid2";
 import ReactMarkdown from 'react-markdown';
 import { useNavigate } from 'react-router-dom';
 
@@ -16,31 +17,37 @@ const Center = (props: ContentProps<TextEntry>) => {
     const navigate = useNavigate();
 
     return (
-        <>
-            {contentEntry.fields.image?.fields.file.url &&
-                <CardMedia
-                    sx={{ width: '100%', height: 'auto' }}
-                    component="img"
-                    src={contentEntry.fields.image.fields.file.url}
-                    alt={contentEntry.fields.image.fields.title}
-                />
-            }
-            {contentEntry.fields.headline &&
-                <Typography align="center" variant="h2" sx={{ pt: { xs: 2, md: 4 }, my: 2 }}>
-                    {contentEntry.fields.headline}
-                </Typography>
-            }
-            <Stack justifyContent="center" direction="column" alignItems="center" spacing={2} sx={{ pb: { xs: 2, md: 4 }, color: 'GrayText' }}>
-                <ReactMarkdown components={Markdown} >
-                    {contentEntry.fields.body}
-                </ReactMarkdown>
-                {contentEntry.fields.ctaLabel &&
-                    <Button onClick={() => navigate(contentEntry.fields.ctaSlug)} variant="contained" sx={{ mt: 4, mb: 4 }}>
-                        {contentEntry.fields.ctaLabel}
-                    </Button>
+        <Grid container direction="column" justifyContent="center" alignItems="center" spacing={2}>
+            <Grid xs={6} sm={6} sx={{ mt: 8, mb: 5 }}>
+                {contentEntry.fields.image?.fields.file.url &&
+                    <CardMedia
+                        sx={{ width: '100%', height: 'auto' }}
+                        component="img"
+                        src={contentEntry.fields.image.fields.file.url}
+                        alt={contentEntry.fields.image.fields.title}
+                    />
                 }
-            </Stack>
-        </>
+            </Grid>
+            <Grid xs={6} sm={8}>
+                {contentEntry.fields.headline &&
+                    <Typography align="center" variant="h2" sx={{ pt: { xs: 2, md: 4 }, my: 2 }}>
+                        {contentEntry.fields.headline}
+                    </Typography>
+                }
+            </Grid>
+            <Grid xs={6} sm={8} >
+                <Stack justifyContent="center" direction="column" alignItems="center" spacing={2} sx={{ pb: { xs: 2, md: 4 }, color: 'GrayText', alignText: 'center' }}>
+                    <ReactMarkdown components={Markdown} >
+                        {contentEntry.fields.body}
+                    </ReactMarkdown>
+                    {contentEntry.fields.ctaLabel &&
+                        <Button onClick={() => navigate(contentEntry.fields.ctaSlug)} variant="contained" sx={{ mt: 4, mb: 4 }}>
+                            {contentEntry.fields.ctaLabel}
+                        </Button>
+                    }
+                </Stack>
+            </Grid>
+        </Grid>
     )
 }
 
