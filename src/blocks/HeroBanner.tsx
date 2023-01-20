1f8bc8c9a39bbe4b3db3c7deea084606342652bf
Your Name <you@example.com>
2023-01-20 15:19:37 +1300

    possible buttons

--- src/blocks/HeroBanner.tsx
+++ src/blocks/HeroBanner.tsx
@@ -6,7 +6,7 @@ import Container from "@mui/material/Container";
 import Stack from '@mui/material/Stack'
 import Typography from "@mui/material/Typography";
 import Grid from '@mui/material/Unstable_Grid2';
-// import { Box } from '@mui/system';
+
 
 import type { ContentProps, HeroBannerEntry } from '@/types';
 
@@ -17,14 +17,19 @@ const HeroBanner = (props: ContentProps<HeroBannerEntry>) => {
         <Stack sx={{ py: 12, backgroundColor: 'primary.main' }}>
             <Container maxWidth="lg" sx={{ display: 'flex', flexDirection: 'column', alignItems: "center" }} >
                 <Grid container direction="row" spacing={2} >
-                    <Grid xs={12} sm={6}>
-                        <Typography gutterBottom color="background.default" variant="h1" >
-                            {contentEntry.fields.headline}
-                        </Typography>
-                        <Typography color="background.default" variant="subtitle2">
-                            {contentEntry?.fields.subheader}
-                        </Typography>
-                    
+                    <Grid justifyContent="space-between" container xs={12} sm={6}>
+                        <Stack>
+                            <Typography gutterBottom color="background.default" variant="h1" >
+                                {contentEntry.fields.headline}
+                            </Typography>
+                            <Typography color="background.default" variant="subtitle2">
+                                {contentEntry?.fields.subheader}
+                            </Typography>
+                        </Stack>
+                        {/* <Stack spacing={1} alignItems="center" direction="row">
+                            <Button size="large" variant="contained" color="secondary"> Try now</Button>
+                            <Button size="large" variant="outlined" color="secondary"> Talk to an expert</Button>
+                        </Stack> */}
                     </Grid>
                     <Grid xs={12} sm={6}>
                         <CardMedia
