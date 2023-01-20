5cc6bc1bbc8d7eab53c397d558720dea46f7a3c8
Your Name <you@example.com>
2023-01-20 14:34:12 +1300

    added asset + fixed hero

--- src/blocks/HeroBanner.tsx
+++ src/blocks/HeroBanner.tsx
@@ -1,32 +1,33 @@
 import React from 'react';
 
+// import Button from '@mui/material/Button';
 import CardMedia from '@mui/material/CardMedia';
 import Container from "@mui/material/Container";
 import Stack from '@mui/material/Stack'
 import Typography from "@mui/material/Typography";
 import Grid from '@mui/material/Unstable_Grid2';
+// import { Box } from '@mui/system';
 
 import type { ContentProps, HeroBannerEntry } from '@/types';
 
 const HeroBanner = (props: ContentProps<HeroBannerEntry>) => {
     const { contentEntry } = props;
 
-    console.log()
-
     return (
         <Stack sx={{ py: 12, backgroundColor: 'primary.main' }}>
             <Container maxWidth="lg" sx={{ display: 'flex', flexDirection: 'column', alignItems: "center" }} >
-                <Grid container direction="row" spacing={2} justifyContent="center" alignItems="center">
-                    <Grid sm={6}>
-                        <Typography color="background.default" textAlign="center" variant="h1" >
+                <Grid container direction="row" spacing={2} >
+                    <Grid xs={12} sm={6}>
+                        <Typography gutterBottom color="background.default" variant="h1" >
                             {contentEntry.fields.headline}
                         </Typography>
-                        <Typography textAlign="center" sx={{ py: 3 }} color="background.default" variant="subtitle2">
+                        <Typography color="background.default" variant="subtitle2">
                             {contentEntry?.fields.subheader}
                         </Typography>
+                    
                     </Grid>
-                    <Grid sm={6}>
-                        <CardMedia sx={{ borderRadius: 2, maxWidth: '44rem', px: { xs: 2, sm: 4, md: 0 } }}
+                    <Grid xs={12} sm={6}>
+                        <CardMedia
                             component="img" loading='lazy' image={contentEntry.fields.heroImage?.fields.file.url}
                             alt={contentEntry.fields.heroImage?.fields.title} />
                     </Grid>
