58b999289c98fafa2dcc60c3823065385e9ba6f7
finest08 <mr.mitcholl@gmail.com>
2023-01-24 15:54:02 +1300

    footer margin

--- src/Viewport/Footer.tsx
+++ src/Viewport/Footer.tsx
@@ -6,20 +6,20 @@ import Stack from '@mui/material/Stack';
 import Typography from '@mui/material/Typography';
 
 const array = [
-    { title: "Contact us", link : "/contact" },
+    { title: "Contact us", link: "/contact" },
 ]
 
 export const Footer = () => {
     const date = new Date().getFullYear();
 
     return (
-        <Stack component="footer" sx={{ py: 6, bgcolor: 'primary.main' }} alignItems="center" justifyContent="center" spacing={2}>
+        <Stack component="footer" sx={{ py: 6, mt: 15, bgcolor: 'primary.main' }} alignItems="center" justifyContent="center" spacing={2}>
             <Box component="footer" sx={{ display: 'flex', justifyContent: 'center' }}>
                 {array.map((item, i) =>
                     <Button key={i} sx={{ my: 2, mx: 0.5, color: 'primary.contrastText' }}>
                         {item.title}
                     </Button>
-                )}    
+                )}
             </Box >
             <Stack alignItems="center" spacing={2}>
                 <Typography align="center" color="primary.contrastText" variant="body2">
