60f47e1b68c0c238dcb5d781051fb22bb08ef0fc
finest08 <mr.mitcholl@gmail.com>
2023-01-27 12:32:05 +1300

    added new text column

--- src/blocks/Section/Center.tsx
+++ src/blocks/Section/Center.tsx
@@ -16,40 +16,59 @@ const Center = (props: ContentProps<TextEntry>) => {
     const navigate = useNavigate();
 
     return (
-        <Container maxWidth="lg" sx={{ px: { xs: 4, sm: 6 }, py: 6 }}>
-            <Grid container direction="column" justifyContent="center" alignItems="center" spacing={2}>
-                <Grid xs={12} md={10}>
-                    {contentEntry.fields.headline &&
-                        <Typography align="center" variant="h2" >
-                            {contentEntry.fields.headline}
-                        </Typography>
-                    }
-                </Grid>
-                <Grid xs={12} md={8}>
-                    {contentEntry.fields.image?.fields.file.url &&
-                        <CardMedia
-                            sx={{ width: '100%', height: 'auto' }}
-                            component="img"
-                            src={contentEntry.fields.image.fields.file.url}
-                            alt={contentEntry.fields.image.fields.title}
-                        />
-                    }
-                </Grid>
-                <Grid xs={12} md={8}>
-                    <Stack justifyContent="center" direction="column" alignItems="center" spacing={2} sx={{ pb: { xs: 2, md: 4 }, color: 'GrayText' }}>
-                        <Typography color="grayText" sx={{ lineHeight: 1.75 }} paragraph align="center" variant="body1" >
-                            {contentEntry.fields.body}
-                        </Typography>
-                        {contentEntry.fields.ctaLabel &&
-                            <Button onClick={() => navigate(contentEntry.fields.ctaSlug)} variant="contained" sx={{ mt: 4, mb: 4 }}>
-                                {contentEntry.fields.ctaLabel}
-                            </Button>
+        <>
+            <Container maxWidth="lg" sx={{ px: { xs: 4, sm: 6 }, py: 6 }}>
+                <Grid container direction="column" justifyContent="center" alignItems="center" spacing={2}>
+                    <Grid xs={12} md={10}>
+                        {contentEntry.fields.headline &&
+                            <Typography align="center" variant="h2" >
+                                {contentEntry.fields.headline}
+                            </Typography>
                         }
-                    </Stack>
+                    </Grid>
+                    <Grid xs={12} md={8}>
+                        {contentEntry.fields.image?.fields.file.url &&
+                            <CardMedia
+                                sx={{ width: '100%', height: 'auto' }}
+                                component="img"
+                                src={contentEntry.fields.image.fields.file.url}
+                                alt={contentEntry.fields.image.fields.title}
+                            />
+                        }
+                    </Grid>
                 </Grid>
-            </Grid>
-        </Container >
+            </Container >
+            <Container maxWidth="xl" sx={{ px: { xs: 4, sm: 6 } }}>
+                <Grid container direction="row" justifyContent="center" alignItems="center" spacing={5}>
+                    <Grid xs={12} md={4}>
+                        <Stack direction="row" justifyContent="center" alignItems="center" spacing={2} sx={{ pb: { xs: 2, md: 4 }, color: 'GrayText' }}>
+                            <Typography color="grayText" sx={{ lineHeight: 1.75 }} paragraph align="center" variant="body1" >
+                                {contentEntry.fields.body}
+                            </Typography>
+                            {contentEntry.fields.ctaLabel &&
+                                <Button onClick={() => navigate(contentEntry.fields.ctaSlug)} variant="contained" sx={{ mt: 4, mb: 4 }}>
+                                    {contentEntry.fields.ctaLabel}
+                                </Button>
+                            }
+                        </Stack>
+                    </Grid>
+                    <Grid xs={12} md={4}>
+                        <Stack direction="row" justifyContent="center" alignItems="center" spacing={2} sx={{ pb: { xs: 2, md: 4 }, color: 'GrayText' }}>
+                            <Typography color="grayText" sx={{ lineHeight: 1.75 }} paragraph align="center" variant="body1" >
+                                {contentEntry.fields.body}
+                            </Typography>
+                            {contentEntry.fields.ctaLabel &&
+                                <Button onClick={() => navigate(contentEntry.fields.ctaSlug)} variant="contained" sx={{ mt: 4, mb: 4 }}>
+                                    {contentEntry.fields.ctaLabel}
+                                </Button>
+                            }
+                        </Stack>
+                    </Grid>
+                </Grid>
+            </Container>
+        </>
     )
+
 }
 
 export default Center;
