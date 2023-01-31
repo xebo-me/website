40fc2d335a94ebb315bf49ce8565cbb2814e2e49
finest08 <mr.mitcholl@gmail.com>
2023-01-31 16:26:44 +1300

    text align with images

--- src/blocks/Section/Left.tsx
+++ src/blocks/Section/Left.tsx
@@ -15,26 +15,27 @@ const Left = (props: ContentProps<TextEntry>) => {
         <Container maxWidth="lg" sx={{ py: 6 }}>
             <Grid container direction="row" justifyContent="center" alignItems="center" spacing={2} sx={{ px: 4 }}>
                 <Grid xs={12} md={8}>
-                    <Typography  variant="h3" sx={{ py: 4, display: { xs: 'flex', md: 'none' } }}>
+                    <Typography variant="h3" sx={{ py: 4, display: { xs: 'flex', md: 'none' } }}>
                         {contentEntry.fields.headline}
                     </Typography>
                     <Box sx={{ justifyContent: 'left', alignItems: 'left' }}>
                         {contentEntry.fields.image?.fields.file.url &&
                             <CardMedia
                                 loading="lazy"
-                            sx={{ objectFit: 'contain', maxWidth: '50%', float: 'right', shapeOutside: 'padding-box' }}
+                                sx={{ maxWidth: '50%', float: 'right', pt: { xs: 1, md: 5 }, px: 2 }}
                                 component="img"
                                 src={contentEntry.fields.image.fields.file.url}
                                 alt={contentEntry.fields.image.fields.title}
                             />
                         }
-                        <Typography variant="h3" sx={{ py: 4, display: { xs: 'none', md: 'flex' }, textAlign: { xs: 'right', md: 'left' } }}>
-                            {contentEntry.fields.headline}
-                        </Typography>
-                        <Typography color="grayText" align='left' sx={{ lineHeight: 1.75 }} variant="body1">
-                            {contentEntry.fields.body}
-                        </Typography>
+
                     </Box>
+                    <Typography variant="h3" sx={{ py: 4, display: { xs: 'none', md: 'flex' }, textAlign: { xs: 'right', md: 'left' } }}>
+                        {contentEntry.fields.headline}
+                    </Typography>
+                    <Typography color="grayText" sx={{ lineHeight: 1.75, textAlign: 'justify', display: 'inline' }} variant="body1">
+                        {contentEntry.fields.body}
+                    </Typography>
                 </Grid>
             </Grid >
         </Container>
--- src/blocks/Section/Right.tsx
+++ src/blocks/Section/Right.tsx
@@ -16,20 +16,20 @@ const Right = (props: ContentProps<TextEntry>) => {
         <Container maxWidth="lg" sx={{ py: 6 }}>
             <Grid container direction="column" justifyContent="center" alignItems="center" spacing={2} sx={{ px: 4 }}>
                 <Grid xs={12} md={8}>
-                    <Typography align="left" variant="h3" sx={{ py: 4, display: { xs: 'flex', md: 'none'} }}>
+                    <Typography align="left" variant="h3" sx={{ py: 4, display: { xs: 'flex', md: 'none' } }}>
                         {contentEntry.fields.headline}
                     </Typography>
                     <Box sx={{ justifyContent: 'left', alignItems: 'left' }}>
                         {contentEntry.fields.image?.fields.file.url &&
                             <CardMedia
                                 loading="lazy"
-                            sx={{ objectFit: 'contain', maxWidth: '50%', float: 'left', shapeOutside: 'padding-box' }}
+                                sx={{ objectFit: 'contain', maxWidth: '50%', float: 'left', mt: { xs: 1, md: 5 }, px: 2 }}
                                 component="img"
                                 src={contentEntry.fields.image.fields.file.url}
                                 alt={contentEntry.fields.image.fields.title}
                             />
                         }
-                        <Typography variant="h3" sx={{ py: 4, display: { xs: 'none', md: 'flex' }, textAlign: {xs: 'left', md: 'right'} }}>
+                        <Typography variant="h3" sx={{ py: 4, display: { xs: 'none', md: 'flex' }, textAlign: { xs: 'left', md: 'right' } }}>
                             {contentEntry.fields.headline}
                         </Typography>
                         <Typography color="grayText" align='right' sx={{ lineHeight: 1.75 }} variant="body1">
