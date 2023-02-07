1914bd4d69476175a18a7cb9240115d10e022a20
finest08 <mr.mitcholl@gmail.com>
2023-02-07 15:57:14 +1300

    adjusted size for TextImage

--- src/views/Content/blocks/TextBlock/TextImage.tsx
+++ src/views/Content/blocks/TextBlock/TextImage.tsx
@@ -2,12 +2,13 @@ import React from 'react';
 
 import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
 import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
 
 import type { ContentProps, TextBlockEntry } from '@/types';
 
 const imgsx = {
-    pl: 6,
+    pl: 4,
     pr: 0,
     float: 'right',
 }
@@ -23,25 +24,27 @@ export const TextImage = (props: ContentProps<TextBlockEntry>) => {
     }
 
     return (
-        <Box sx={{ width: '100%' }}>
-            {contentEntry.fields.image?.fields.file.url &&
-                <Box component="img"
-                    src={contentEntry.fields.image.fields.file.url}
-                    loading="lazy"
-                    sx={{ objectFit: 'contain', maxWidth: '50%', float: imgsx.float, pl: imgsx.pl, pr: imgsx.pr, pb: 2 }}
-                    alt={contentEntry.fields.image.fields.title}
-                />
-            }
-            <Typography align="left" variant="h3">
-                {contentEntry.fields.title}
-            </Typography>
-            <Typography variant="body1" component="div" color="grayText" align='right' sx={{
-                lineHeight: 1.75,
-                textAlign: 'justify'
-            }}>
-                {documentToReactComponents(contentEntry.fields.body)}
-            </Typography>
-        </Box>
+        <Container maxWidth="md">
+            <Box sx={{ width: '100%', px: 4 }}>
+                {contentEntry.fields.image?.fields.file.url &&
+                    <Box component="img"
+                        src={contentEntry.fields.image.fields.file.url}
+                        loading="lazy"
+                        sx={{ objectFit: 'contain', maxWidth: '50%', float: imgsx.float, pl: imgsx.pl, pr: imgsx.pr, pb: 2 }}
+                        alt={contentEntry.fields.image.fields.title}
+                    />
+                }
+                <Typography align="left" variant="h3">
+                    {contentEntry.fields.title}
+                </Typography>
+                <Typography variant="body1" component="div" color="grayText" align='right' sx={{
+                    lineHeight: 1.75,
+                    textAlign: 'justify'
+                }}>
+                    {documentToReactComponents(contentEntry.fields.body)}
+                </Typography>
+            </Box>
+        </Container>
     )
 }
 export default TextImage;
